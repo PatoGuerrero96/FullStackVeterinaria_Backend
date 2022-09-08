c6e92a387e13bd82169523e49a55c3f22754b354
Patricio Guerrero <gpatricio515@gmail.com>
2022-09-08 14:37:52 -0300

    V5

--- index.js
+++ index.js
@@ -25,7 +25,5 @@ app.use(cors(corsOptions));
 
 app.use("/api/veterinarios", veterinarioRoutes );
 app.use("/api/pacientes", pacienteRoutes );
-const PORT = process.env.PORT || 5000;
-app.listen(5000, ()=>{
-    console.log("Servidor funcionando desde el puerto "+PORT)
-    })
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
